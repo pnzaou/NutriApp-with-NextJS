d079256fe8561a462701be3eb5ca71fa75f37121
pnzaou <perrinemmanuelnzaou@gmail.com>
2025-03-26 13:41:17 +0000

    Modification de la route /api/foods/[name]

--- src/app/api/foods/[name]/route.ts
+++ src/app/api/foods/[name]/route.ts
@@ -1,20 +1,19 @@
-import { foods } from "@/data";
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+import { foods } from "@/data"
+import { NextResponse } from "next/server"
 
-export const GET = async (_req: NextRequest, context: { params: { name: string } }) => {
-    const { name } = context.params;
-    const index = foods.findIndex((food) => food.name.toLowerCase().replace(/ /g, '-') === name);
+export const GET = async (req: Request, { params }) => {
+    const { name } = await params
+    const index = foods.findIndex((food) => food.name.toLowerCase().replace(/ /g, '-') === name)
 
-    if (index === -1) {
+    if(index === -1) {
         return NextResponse.json(
-            { message: "Aliment introuvable!" },
-            { status: 404 }
-        );
+            {message: "Aliment introuvable!"},
+            {status: 404}
+        )
     }
 
     return NextResponse.json(
-        { data: foods[index] },
-        { status: 200 }
-    );
-};
+        {data: foods[index]},
+        {status: 200}
+    )
+}
